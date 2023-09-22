35e128861c58d8264269c5f733d4086b4740e195
guangying666 <3251132845@qq.com>
2023-09-22 18:18:32 +0800

    第一次提交

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class DesEncode:
+    def __int__(self):
+        self.ip = [2, 6, 3, 1, 4, 8, 5, 7]  # 初始置换盒
+        self.ip1 = [4, 1, 3, 5, 7, 2, 8, 5, 7]  # 最终置换盒
 
+        self.EpBox = [4, 1, 2, 3, 2, 3, 4, 1]  # E扩展置换
+        self.Sbox1 = [
+            [1, 0, 3, 2],
+            [3, 2, 1, 0],
+            [0, 2, 1, 3],
+            [3, 1, 0, 2]
+        ]  # 第一个S盒替代
+        self.Sbox2 = [
+            [0, 1, 2, 3],
+            [2, 3, 1, 0],
+            [3, 0, 1, 2],
+            [2, 1, 0, 3]
+        ]  # 第二个S盒替代
+        self.SPbox = [2, 4, 3, 1]  # P盒置换
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        self.P10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+        self.P8 = [6, 3, 7, 4, 8, 5, 10, 9]
+        self.LeftShiFt1 = [2, 3, 4, 5, 1]
+        self.LeftShiFt2 = [3, 4, 5, 1, 2]
